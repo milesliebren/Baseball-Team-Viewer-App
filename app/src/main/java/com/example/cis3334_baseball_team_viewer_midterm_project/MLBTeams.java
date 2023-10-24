a818253b7319c492f051da4db954b95a1f1607c2
Miles Liebren <mliebren@css.edu>
2023-10-24 10:08:21 -0500

    v2.3 (Not working -- Added Tom's Code)

---
+++ app/src/main/java/com/example/cis3334_baseball_team_viewer_midterm_project/MLBTeams.java
@@ -0,0 +1,66 @@
+package com.example.cis3334_baseball_team_viewer_midterm_project;
+
+import java.util.ArrayList;
+public class MLBTeams {
+    private String copyright;
+    ArrayList < Team > teams = new ArrayList < Team > ();
+    public class Team{
+        public String allStarStatus;
+        public int id;
+        public String name;
+        public String link;
+        public int season;
+        public Venue venue;
+        public String teamCode;
+        public String fileCode;
+        public String abbreviation;
+        public String teamName;
+        public String locationName;
+        public String firstYearOfPlay;
+        public League league;
+        public Division division;
+        public Sport sport;
+        public String shortName;
+        public String parentOrgName;
+        public int parentOrgId;
+        public String franchiseName;
+        public String clubName;
+        public boolean active;
+        public SpringLeague springLeague;
+        public SpringVenue springVenue;
+    }
+    public class Venue{
+        public int id;
+        public String name;
+        public String link;
+    }
+    public class Division{
+        public int id;
+        public String name;
+        public String link;
+    }
+    public class League{
+        public int id;
+        public String name;
+        public String link;
+    }
+    public class Root{
+        public String copyright;
+        public ArrayList<Team> teams;
+    }
+    public class Sport{
+        public int id;
+        public String link;
+        public String name;
+    }
+    public class SpringLeague{
+        public int id;
+        public String name;
+        public String link;
+        public String abbreviation;
+    }
+    public class SpringVenue{
+        public int id;
+        public String link;
+    }
+}
--- app/src/main/java/com/example/cis3334_baseball_team_viewer_midterm_project/MainActivity.java
+++ app/src/main/java/com/example/cis3334_baseball_team_viewer_midterm_project/MainActivity.java
@@ -4,25 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+import com.google.gson.Gson;
 
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONObject;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
 {
-    private TabItem tabMLB;
-    private TabItem tabTripleA;
-    private TabItem tabDoubleA;
-    private RecyclerView recyclerView;
+    TabItem tabMLB;
+    TabItem tabTripleA;
+    TabItem tabDoubleA;
+    RecyclerView recyclerView;
+    TextView textViewStatus;
     MainViewModel viewModel;
-    private TeamAdapter teamAdapter;
+    TeamAdapter teamAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +45,9 @@ public class MainActivity extends AppCompatActivity
         teamAdapter = new TeamAdapter(this.getApplication(), viewModel);
 
         recyclerView = findViewById(R.id.recyclerView);
+        textViewStatus = findViewById(R.id.textViewStatus);
         recyclerView.setAdapter(teamAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
 
         setUpTabListeners();
         observeData();
@@ -86,5 +98,37 @@ public class MainActivity extends AppCompatActivity
                 teamAdapter.submitList(teams);
             }
         });
+    }
+
+    private void getStudentAPI() {
+        // ======================= Student must add code here to get JSON data from an API =======================
+        // Define URL to use. Using Cat Facts API here. Note number of facts set to 1
+        // ---- Remember to add the following permission to the AndroidManifest.xml file
+        //      <uses-permission android:name="android.permission.INTERNET" />
+        String url = "https://statsapi.mlb.com/api/v1/teams";
+        Log.d("CIS 3334", "url = "+url);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        textViewStatus.setText("MLB Received Response");
+                        String jsonFact= response.toString();
+                        Gson gson = new Gson();
+                        MLBTeams MLBdata = gson.fromJson(jsonFact, MLBTeams.class);
+                        textViewStatus.setText("Number of teams received = "+MLBdata.teams.size());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("CIS 3334", "In MLB -- onErrorResponse = "+error);
+
+                    }
+                });
+
+        // Create a RequestQueue used to send web requests using Volley
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+
     }
 }
